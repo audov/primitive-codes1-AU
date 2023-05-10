1d71e4f2f7b1e194d49edde0f30f9e25b954197b
Neto test <audov@mail.ru>
2023-05-10 09:43:51 +0300

    camelcase is applied

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int bb = 1000; //начальный баланс на счету клиента
-        int amount_in = 4500; // сумма пополнения
-        int bonus_criteria = 100; // ставка бонуса
-        int bonus_rate = 1; // бонус в рублях за каждые 100 рублей пополнения
-        int bonus_amount = amount_in / bonus_criteria * bonus_rate; //итоговая сумма бонуса
-        int amount_out = bb + amount_in + bonus_amount; // итоговая сумма на счету
+        int amountIn = 4500; // сумма пополнения
+        int bonusCriteria = 100; // ставка бонуса
+        int bonusRate = 1; // бонус в рублях за каждые 100 рублей пополнения
+        int bonusAmount = amountIn / bonusCriteria * bonusRate; //итоговая сумма бонуса
+        int amountOut = bb + amountIn + bonusAmount; // итоговая сумма на счету
 
-        if (amount_in > 1000) {
-            System.out.println("Сумма бонуса за пополнение составила: " + bonus_amount);
+        if (amountIn > 1000) {
+            System.out.println("Сумма бонуса за пополнение составила: " + bonusAmount);
         } else {
             System.out.println("Сумма бонуса за пополнение составила: 0");
         }
-        System.out.println("Итоговая сумма на счету: " + amount_out);
+        System.out.println("Итоговая сумма на счету: " + amountOut);
 
     }
 }
